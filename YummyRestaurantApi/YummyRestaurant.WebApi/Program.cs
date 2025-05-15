be56b6af21bf0647de70d81e0bab9a3d2d0401dd
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-05-15 14:20:10 +0300

    feat(backend): add Categories API and configure Swagger
    - Updated `Program.cs` to register `YummyRestaurantApiContext` and add controllers.
    - Modified `YummyRestaurant.WebApi.csproj` to remove the controllers folder.
    - Added `CategoriesController` for managing categories with CRUD operations using the database context.

--- YummyRestaurantApi/YummyRestaurant.WebApi/Program.cs
+++ YummyRestaurantApi/YummyRestaurant.WebApi/Program.cs
@@ -1,6 +1,10 @@
+using YummyRestaurant.WebApi.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<YummyRestaurantApiContext>(); // Register the database context with the dependency injection container
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
