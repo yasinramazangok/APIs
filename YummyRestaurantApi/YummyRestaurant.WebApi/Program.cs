048eb1c6acba74c10d4e307f5c613d8109bff49b
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-05-16 15:52:26 +0300

    feat(backend): add ProductsController with CRUD operations and FluentValidation
    - This commit introduces a new `ProductsController` for managing product data, implementing CRUD operations.
    - It integrates FluentValidation for input validation and AutoMapper for object mapping.
    - A `ProductValidator` class is added to enforce validation rules for the `Product` entity.
    - Additionally, the project file is updated to include FluentValidation and AutoMapper dependencies, and the `Program.cs` file is modified to register these services in the dependency injection container.
    
    <-- This commit message was generated with GitHub Copilot in Visual Studio. -->

--- YummyRestaurantApi/YummyRestaurant.WebApi/Program.cs
+++ YummyRestaurantApi/YummyRestaurant.WebApi/Program.cs
@@ -1,11 +1,17 @@
+using FluentValidation;
 using System.Reflection;
 using YummyRestaurant.WebApi.Contexts;
+using YummyRestaurant.WebApi.Entities;
+using YummyRestaurant.WebApi.ValidationRules;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddDbContext<YummyRestaurantApiContext>(); // Register the database context with the dependency injection container
+
+builder.Services.AddScoped<IValidator<Product>, ProductValidator>();
+
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // Register AutoMapper with the current assembly
 
 builder.Services.AddControllers();
---
+++ YummyRestaurantApi/YummyRestaurant.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿using YummyRestaurant.WebApi.Entities;
+using FluentValidation;
+
+namespace YummyRestaurant.WebApi.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün adını boş geçmeyin!");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("En az 2 karakter veri girişi yapın!");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("En fazla 50 karakter veri girişi yapın!");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez").GreaterThan(0).WithMessage("Ürün fiyatı negatif olamaz").LessThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz, girdiğiniz değeri kontrol edin!");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Ürün açıklaması boş geçilemez!");
+        }
+    }
+}
