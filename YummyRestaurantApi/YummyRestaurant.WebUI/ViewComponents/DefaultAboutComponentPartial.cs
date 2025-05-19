83ec9b4465cc29d3fba4fbd591f339dea0e2c4ac
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-05-19 16:27:45 +0300

    feat(backend): create project setup for YummyRestaurant.WebUI
    - Created project files and template.

---
+++ YummyRestaurantApi/YummyRestaurant.WebUI/ViewComponents/DefaultAboutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YummyRestaurant.WebUI.ViewComponents
+{
+    public class DefaultAboutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ YummyRestaurantApi/YummyRestaurant.WebUI/ViewComponents/DefaultFeatureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YummyRestaurant.WebUI.ViewComponents
+{
+    public class DefaultFeatureComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
