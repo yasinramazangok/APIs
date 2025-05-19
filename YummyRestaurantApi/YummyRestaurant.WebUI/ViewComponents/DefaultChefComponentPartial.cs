ba7909b9432e9e7ef9eab9742943c45398be34d3
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-05-19 22:43:06 +0300

    feat(frontend): add chef display component and related DTOs
    - Implemented a new `DefaultChefComponentPartial` to dynamically render chef information on the website.
    - Updated `Index.cshtml` to replace the old chefs section with the new component.
    - Added `ResultChefDto` for chef data representation and included necessary using directives in `_ViewImports.cshtml`.
    - Created a view for the component in `Default.cshtml` to display chef details fetched from the API.

---
+++ YummyRestaurantApi/YummyRestaurant.WebUI/ViewComponents/DefaultChefComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyRestaurant.WebUI.Dtos.ChefDtos;
+
+namespace YummyRestaurant.WebUI.ViewComponents
+{
+    public class DefaultChefComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultChefComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7169/api/Chefs/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
